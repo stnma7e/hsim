5be6b8c956036d32226d76588b6de1002b49aca3
Sam Delmerico <svdelmerico@gmail.com>
2014-02-23 16:46:31 -0500

    events recieved are now in a JSON format; added a tags file
    Event are sent over the network as a string for the name followed by a stream of bytes of the JSON serialized event. The event is transformed into an algebraic data type.

--- Event.hs
+++ Event.hs
@@ -1,16 +1,26 @@
-module Event where
+module Event
+( Event(..)
+, getEvent
+, sendEvent
+) where
 
+import Control.Monad
+import Control.Applicative
+import Control.Monad.Trans
 import System.IO
+
 import Network.Socket hiding (recv, sendAll)
 import Network.Socket.ByteString (recv, sendAll)
-import Data.ByteString.Char8 as B (unpack, pack)
-import Control.Monad
+import Data.ByteString.Char8 as C (unpack, pack)
+
 import Text.JSON
-import Text.JSON.String
-import qualified Numeric.Matrix as Mat
 import Data.List
+import Data.Word
 
 import Instance
+
+jsonTypeField  = "Type"
+jsonEventField = "Event"
 
 data Event = AttackEvent (GOiD, GOiD)
            | CharacterMovedEvent GOiD [Float]
@@ -21,58 +31,63 @@ data Event = AttackEvent (GOiD, GOiD)
 
 instance JSON Event where
     readJSON = undefined
-    showJSON (AttackEvent (char1, char2)) = buildEventJSON "attack" ((show char1)++" "++(show char2))
-    showJSON (CharacterMovedEvent char loc) = buildEventJSON "characterMoved" ((show char)++" "++(show loc))
-    showJSON (RequestCharacterCreationEvent charType loc) = buildEventJSON "requestCharacterCreation" (charType++" "++(show loc))
+    showJSON (AttackEvent (char1, char2)) = buildEventJSON "attack" [("Char1", showJSON char1), ("Char2", showJSON char2)]
+    showJSON (CharacterMovedEvent char loc) = buildEventJSON "characterMoved" [("CharID", showJSON char), ("NewLocation", showJSON loc)]
+    showJSON (RequestCharacterCreationEvent charType loc) = buildEventJSON "requestCharacterCreation" [("Type", showJSON charType), ("Location", showJSON loc)]
+    showJSON evt = error $ "ERROR: no instance showJSON for type: " ++ (show evt)
 
-buildEventJSON :: String -> String -> JSValue
-buildEventJSON typ event = showJSON $ toJSObject [("eventType", typ), ("event", "{"++ event ++"}")]
+buildEventJSON :: String -> [(String, JSValue)] -> JSValue
+buildEventJSON typ event = showJSON $ makeObj [(jsonTypeField, showJSON typ), (jsonEventField, makeObj event)]
 
 data EventDescriptor = EventDescriptor
     { eventType :: String
-    , event :: String
+    , event :: JSValue
     } deriving (Show)
+
+(!) :: JSON a => JSObject JSValue -> String -> Result a
+(!) = flip valFromObj
 
 instance JSON EventDescriptor where
     showJSON = undefined
-    readJSON obj@(JSObject _) = do
-        obj' <- (readJSON obj :: Result (JSObject String))
-        let objList = fromJSObject obj'
-        let eventType = (snd . head $ objList)
-        let event = (snd . head . tail $ objList)
-        return $ EventDescriptor eventType [x | x <- event, x `notElem` ['{','}']]
+    readJSON (JSObject obj) = 
+        let event = fmap (map toEnum) (obj ! jsonEventField :: Result [Int]) :: Result [Char]
+        in case event of
+        (Ok event') -> EventDescriptor   <$>
+                       obj ! jsonTypeField <*>
+                       (decode (event' :: [Char]))
+        (Error err) -> error err
     readJSON _ = mzero
 
 attackEvent :: EventDescriptor -> Event
 attackEvent (EventDescriptor typ event)
     | typ == "attack" =
-        let charList =  map read . words $ event
-        in AttackEvent ((head charList), (head $ tail charList))
-    | typ == "characterMoved" =
-        let goid = read . head . words $ event
-            -- characterMoved event takes the form of "<character GOiD> [x y z]"
-            loc  = map read . words $ filter (`notElem` ['[',']']) (unwords . tail . words $ event)
-        in CharacterMovedEvent goid loc
+        case readJSON event of
+            (Ok (JSObject obj)) -> let (Ok char1) = obj ! "Char1"
+                                       (Ok char2) = obj ! "Char2" 
+                                   in AttackEvent (char1,char2)
+    | typ == "characterMoved" = do
+        case readJSON event of
+            (Ok (JSObject obj)) -> let (Ok goid) = obj ! "CharID"
+                                       (Ok loc)  = obj ! "NewLocation"
+                                   in CharacterMovedEvent goid loc
 
-getEvent :: Socket -> IO Event
+getEvent :: Socket -> IO ()
 getEvent sock = do
     msg <- recv sock 1024
-    putStrLn $ B.unpack msg
-    case decode (B.unpack msg) :: Result EventDescriptor of
+    putStrLn $ "received: " ++ C.unpack msg
+    case decode (C.unpack msg) :: Result EventDescriptor of
         {-(Ok res) -> putStrLn $ show . fromJSObject $ res-}
         (Ok evt)  -> do
             dispatch evt
-            return $ attackEvent evt
-        otherwise -> error "failed in parsing the JSON values"
+        (Error err) -> do
+            putStrLn $ "ERROR: " ++ err
 
 dispatch :: EventDescriptor -> IO ()
-dispatch evt@(EventDescriptor "attack" event) = do
-    putStrLn . show $ attackEvent evt
 dispatch evt = do
-    putStrLn . show $ evt
+    putStrLn . show $ attackEvent evt
 
 sendEvent :: Socket -> Event -> IO ()
 sendEvent sock evt = do
-    let msg = B.pack (encode evt)
-    putStrLn $ B.unpack msg
+    let msg = C.pack (encode evt)
+    putStrLn $ "sending: " ++ C.unpack msg
     sendAll sock msg
