60fb0f1a026216ae775eb14304b35b87bc2caf79
Sam Delmerico <svdelmerico@gmail.com>
2014-03-01 19:18:39 -0500

    remvoed Either constraint on ComponentManager

--- Instance.hs
+++ Instance.hs
@@ -19,7 +19,7 @@ type AiFunction = GOiD -> ComponentManager -> ComponentManager -> State Componen
 
 type Instance         = State InstanceState
 newtype InstanceState = InstanceState
-    { getManagers :: [Either String ComponentManager]
+    { getManagers :: [ComponentManager]
     } deriving (Show)
 
 start :: Instance ()
@@ -32,17 +32,25 @@ loop :: GOiD -> Instance ()
 loop 0 = state $ \s -> ((),s)
 loop n = do
     s <- get
-    put . InstanceState . map update $ getManagers s
+    put . InstanceState . map (\m -> let m' = update m
+                                     in case m' of
+                                         (Right m'') -> m''
+                                         (Left err)  -> error err) $
+        getManagers s
     loop (n-1)
 
 createObject :: GOiD -> Instance ()
 createObject idToMake = state $ \s -> ((), InstanceState $
-    map (createComponent idToMake) $ getManagers s
+    map (\m -> let m' = createComponent idToMake m
+               in case m' of
+                   (Right m'') -> m''
+                   (Left err)  -> error err) $ 
+        getManagers s
     )
 
 addManager :: ComponentManager -> Instance ()
 addManager m = state $ \s -> ((), InstanceState $
-    Right m : getManagers s
+    m : getManagers s
     )
 
 data Component = CharComponent [Int]
@@ -52,14 +60,11 @@ data ComponentManager = TransformManager (Map.Map GOiD (Mat.Matrix Float))
 					    deriving (Show)
 
 class ComponentCreator a where
-	createComponent :: GOiD -> Either String a -> Either String a
-	update :: Either String a -> Either String a
+	createComponent :: GOiD -> a -> Either String a
+	update :: a -> Either String a
 	
 instance ComponentCreator ComponentManager where
-	createComponent id m@(Left err) = error err
-	createComponent id m@(Right (CharManager ids)) = Right . CharManager $ Map.insert id (CharComponent [1,12,4,43,34,3], \gid m1 m2 -> state $ \s -> ((), s)) ids
-	createComponent id (Right (TransformManager mats)) = Right (TransformManager (Map.insert id (Mat.matrix (4,4) (\(_,_) -> 0)) mats))
+	createComponent id (CharManager ids) = Right . CharManager $ Map.insert id (CharComponent [1,12,4,43,34,3], \gid m1 m2 -> state $ \s -> ((), s)) ids
+	createComponent id (TransformManager mats) = Right (TransformManager (Map.insert id (Mat.matrix (4,4) (\(_,_) -> 0)) mats))
 	
-	update (Left err) = error err
-	update m@(Right (CharManager ids)) = m
-	update m@(Right (TransformManager mats)) = m
+	update m = Right m
